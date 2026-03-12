a9f91f6a1d734f9596dc07eb76b8ed79ac944a4e
cayden <schalkcayden@gmail.com>
2026-03-11 23:58:00 -0500

    implemented runtime paths for configuration inside HPC

--- src/configs/base.py
+++ src/configs/base.py
@@ -3,7 +3,9 @@ import os
 from abc import ABC, abstractmethod
 from typing import List, Union
 from pathlib import Path
+
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
 
 class Base(ABC):
     def __init__(self, **kwargs):
@@ -32,7 +34,7 @@ class BaseConfig(Base):
         for k, v in sorted(vars(cfg).items()):
             message += f"{str(k):>30}: {str(v):<40}\n"
 
-        os.makedirs(os.path.join(cfg.checkpoint_dir), exist_ok=True)
+        os.makedirs(cfg.checkpoint_dir, exist_ok=True)
         out_opt = os.path.join(cfg.checkpoint_dir, "cfg.log")
         with open(out_opt, "w") as opt_file:
             opt_file.write(message)
@@ -55,10 +57,8 @@ class BaseConfig(Base):
             elif value == "None":
                 convert_value = None
             elif (
-                value.startswith("'")
-                and value.endswith("'")
-                or value.startswith('"')
-                and value.endswith('"')
+                (value.startswith("'") and value.endswith("'")) or
+                (value.startswith('"') and value.endswith('"'))
             ):
                 convert_value = value[1:-1]
             else:
@@ -67,10 +67,9 @@ class BaseConfig(Base):
 
         with open(cfg_path, "r") as f:
             data = f.read().split("\n")
-            # remove all empty strings
             data = list(filter(None, data))
-            # convert to dict
             data_dict = {}
+
             for i in range(len(data)):
                 key, value = (
                     data[i].split(":")[0].strip(),
@@ -83,12 +82,12 @@ class BaseConfig(Base):
                     value = decode_value(value)
 
                 data_dict[key] = value
+
         for key, value in data_dict.items():
             setattr(self, key, value)
 
 
 class Config(BaseConfig):
-    # Base
     def __init__(self, **kwargs):
         super(Config, self).__init__(**kwargs)
         self.name = "default"
@@ -96,11 +95,17 @@ class Config(BaseConfig):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def set_args(self, **kwargs):
+    def set_args(self):
         # Training settings
-        self.trainer = "Trainer"  # Trainer type use for training model [MSER_Trainer, Trainer, MarginTrainer]
+        self.trainer: str = "Trainer"
         self.num_epochs: int = 100
-        self.checkpoint_dir: str = "checkpoints"
+
+        # runtime path values - these should be injected by train.py
+        self.checkpoint_root: Union[str, None] = None
+        self.checkpoint_dir: Union[str, None] = None
+        self.log_dir: Union[str, None] = None
+        self.weight_dir: Union[str, None] = None
+
         self.save_all_states: bool = False
         self.save_best_val: bool = True
         self.max_to_keep: int = 1
@@ -112,36 +117,33 @@ class Config(BaseConfig):
         self.learning_rate_step_size: int = 30
         self.learning_rate_gamma: float = 0.1
 
-        self.optimizer_type: str = "Adam"  # Adam, SGD, AdamW
-        # Adam config
+        self.optimizer_type: str = "Adam"
         self.adam_beta_1 = 0.9
         self.adam_beta_2 = 0.999
         self.adam_eps = 1e-08
         self.adam_weight_decay = 0
-        # SGD config
+
         self.momemtum = 0.99
         self.sdg_weight_decay = 1e-6
 
         # Resume training
         self.resume: bool = False
-        # path to checkpoint.pt file, only available when using save_all_states = True in previous training
         self.resume_path: Union[str, None] = None
         self.cfg_path: Union[str, None] = None
-        if self.resume:
-            assert os.path.exists(str(self.resume_path)), "Resume path not found"
 
         self.loss_type: str = "CrossEntropyLoss"
 
         # Dataset
-        self.data_name: str = (
-            "MELD"  # [IEMOCAP, ESD, MELD, IEMOCAPAudio, IEMOCAP_MSER]
-        )
-        self.data_root: str = (
-            str(PROJECT_ROOT / "data" / "MELD_preprocessed") # folder contains train.pkl and test.pkl
-        )
-        self.data_valid: str = (
-            "val.pkl"  # change this to your validation subset name if you want to use validation dataset. If None, test.pkl will be use
-        )
+        self.data_name: str = "MELD"
+
+        self.raw_root: Union[str, None] = None
+        self.processed_root: Union[str, None] = None
+
+        # split names
+        self.data_train: str = "train.pkl"
+        self.data_valid: str = "val.pkl"
+        self.data_test: str = "test.pkl"
+
         self.num_workers = 0
 
         # use for training with batch size > 1
@@ -152,8 +154,8 @@ class Config(BaseConfig):
         self.num_classes: int = 4
         self.num_attention_head: int = 8
         self.dropout: float = 0.05
-        self.model_type: str = "TriMemoCMT"  #
-        self.text_encoder_type: str = "bert"  # [bert, roberta]
+        self.model_type: str = "TriMemoCMT"
+        self.text_encoder_type: str = "bert"
         self.text_encoder_dim: int = 768
         self.text_unfreeze: bool = False
         self.audio_encoder_type: str = "hubert_base"
@@ -170,5 +172,6 @@ class Config(BaseConfig):
         # Search for linear layer output dimension
         self.linear_layer_output: List = [128]
         self.linear_layer_last_dim: int = 64
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+
+        if self.resume and self.resume_path is not None:
+            assert os.path.exists(str(self.resume_path)), "Resume path not found"
